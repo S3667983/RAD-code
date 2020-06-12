4a5c27f1e67f0b662b36b9c26cd64c01b028de2c
s3667983 <s3667983@student.rmit.edu.au>
2020-06-13 01:50:53 +1000

    heroku 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,11 @@ Rails.application.routes.draw do
   root 'users#signup'
   get 'users/new'
   get 'session/new'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
-  resources :microposts, only: [:create, :destroy]
 
 end
