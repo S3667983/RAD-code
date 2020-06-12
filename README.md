7f54097ed4b5b298897121301193fed08cea0430
s3667983 <s3667983@student.rmit.edu.au>
2020-06-13 02:34:17 +1000

    Deployment

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
 Andrew Harris: s3667983
-Ethan Cutting: s3660434
 
 Pass is the furthers
 
-// 
-completed sign up
-then go to login
-put detial
-then you will be in main
-
 http://localhost:3000/signup
 
-https://dashboard.heroku.com/apps/aqueous-eyrie-78857
-https://aqueous-eyrie-78857.herokuapp.com/
 
-https://github.com/S3667983/RAD2020_s3660434_s3667983
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_101805) do
+ActiveRecord::Schema.define(version: 2020_06_12_162810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "value"
+    t.string "abbr"
+    t.integer "offset"
+    t.boolean "isdst"
+    t.string "text"
+    t.string "utc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
